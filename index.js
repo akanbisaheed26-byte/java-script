3cd89e5e94473b1775719e4f8029a1dc7f72efe7
akanbisaheed26-byte <akanbisaheed26@gmail.com>
2026-09-20 20:44:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+let firstName = "Saheed";
+let isMarried = false;
+let job;
+let emptyValue = null;
+
+console.log(typeof firstName);
+console.log(typeof isMarried);
+console.log(typeof job);
+console.log(typeof emptyValue);
+
+
+let variableOne;
+let variableTwo;
+let variableThree;
+let variableFour
+
+let city = "Ilorin";
+let temperature = 20;
+let hasLicense = true;
+let favoriteColor = "blue";
+
+
+let myFirstName = "Saheed";
+let myLastName = "ola";
+let myMaritalStatus = "single";
+let myCountry = "Nigeria";
+let myAge = 18;
+
+
+let yourFirstName = "Saheed",
+    yourLastName = "mide",
+    yourMaritalStatus = "single",
+    yourCountry = "Nigeria",
+    yourAge = 30;
+
+
+console.log(`I am ${18} years old.`);
+console.log(`You are ${30} years old.`);
