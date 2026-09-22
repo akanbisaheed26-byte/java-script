b6edbdceaf30c5a1e0b9f7a06027cd98265e6702
akanbisaheed26-byte <akanbisaheed26@gmail.com>
2026-09-22 15:23:19 +0100

    Add files via upload

--- Day-2-Exercise/index.js
+++ Day-2-Exercise/index.js
@@ -1,278 +1,243 @@
-const output = [];
-
-function show(title, value) {
-  console.log(title, value);
-  output.push(`<p><strong>${title}</strong> ${format(value)}</p>`);
-}
-
-function format(value) {
-  if (Array.isArray(value)) {
-    return `[${value.map(item => JSON.stringify(item)).join(", ")}]`;
-  }
-
-  if (value === null) {
-    return "null";
-  }
-
-  if (typeof value === "object") {
-    return JSON.stringify(value);
-  }
-
-  return String(value)
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;");
-}
-
 // ==================== LEVEL 1 ====================
 
-const challenge = "30 Days Of JavaScript";
+let challenge = "30 Days Of JavaScript";
 
-const spaceSeparated = challenge.split(" ");
+console.log("1. Challenge:", challenge);
 
-const becauseSentence =
-  "You cannot end a sentence with because because because is a conjunction";
+console.log("2. Length:", challenge.length);
 
-show("1. Challenge", challenge);
+console.log("3. Uppercase:", challenge.toUpperCase());
 
-show("2. Length", challenge.length);
+console.log("4. Lowercase:", challenge.toLowerCase());
 
-show("3. Uppercase", challenge.toUpperCase());
+console.log("5. First word:", challenge.substring(0, 2));
 
-show("4. Lowercase", challenge.toLowerCase());
+console.log("6. Phrase Days Of JavaScript:", challenge.substring(3));
 
-show("5. First word", challenge.substr(0, 2));
+console.log("7. Does it contain Script?:", challenge.includes("Script"));
 
-show("6. Phrase Days Of JavaScript", challenge.slice(3));
+console.log("8. Split into an array:", challenge.split(""));
 
-show("7. Contains Script", challenge.includes("Script"));
+console.log("9. Split at spaces:", challenge.split(" "));
 
-show("8. Split into an array", challenge.split(""));
+let companies =
+  "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
 
-show("9. Split at spaces", spaceSeparated);
+console.log("10. Companies array:", companies.split(", "));
 
-show(
-  "10. Companies array",
-  "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(", ")
-);
-
-show(
-  "11. Replace JavaScript with Python",
+console.log(
+  "11. Replace JavaScript with Python:",
   challenge.replace("JavaScript", "Python")
 );
 
-show("12. Character at index 15", challenge.charAt(15));
+console.log(
+  "12. Character at index 15:",
+  challenge.charAt(15)
+);
 
-show(
-  "13. Character code of J",
+console.log(
+  "13. Character code of J:",
   challenge.charCodeAt(challenge.indexOf("J"))
 );
 
-show("14. First occurrence of a", challenge.indexOf("a"));
-
-show("15. Last occurrence of a", challenge.lastIndexOf("a"));
-
-show(
-  "16. First occurrence of because",
-  becauseSentence.indexOf("because")
+console.log(
+  "14. First occurrence of a:",
+  challenge.indexOf("a")
 );
 
-show(
-  "17. Last occurrence of because",
-  becauseSentence.lastIndexOf("because")
+console.log(
+  "15. Last occurrence of a:",
+  challenge.lastIndexOf("a")
 );
 
-show(
-  "18. Search for because",
-  becauseSentence.search("because")
+let sentence =
+  "You cannot end a sentence with because because because is a conjunction";
+
+console.log(
+  "16. First occurrence of because:",
+  sentence.indexOf("because")
 );
 
-show(
-  "19. Trimmed string",
+console.log(
+  "17. Last occurrence of because:",
+  sentence.lastIndexOf("because")
+);
+
+console.log(
+  "18. Search for because:",
+  sentence.search("because")
+);
+
+console.log(
+  "19. Trimmed string:",
   " 30 Days Of JavaScript ".trim()
 );
 
-show(
-  "20. Starts with 30 Days Of JavaScript",
+console.log(
+  "20. Starts with 30 Days Of JavaScript:",
   challenge.startsWith("30 Days Of JavaScript")
 );
 
-show(
-  "21. Ends with 30 Days Of JavaScript",
+console.log(
+  "21. Ends with 30 Days Of JavaScript:",
   challenge.endsWith("30 Days Of JavaScript")
 );
 
-show(
-  "22. All a characters",
+console.log(
+  "22. All the letter a characters:",
   challenge.match(/a/gi)
 );
 
-show(
-  "23. Concatenated string",
+console.log(
+  "23. Concatenated string:",
   "30 Days Of".concat(" ", "JavaScript")
 );
 
-show(
-  "24. Repeated twice",
+console.log(
+  "24. Repeated twice:",
   challenge.repeat(2)
 );
 
+
 // ==================== LEVEL 2 ====================
 
-show(
-  "25. John Holmes quote",
+console.log(
+  "25. John Holmes quote:",
   "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
 );
 
-show(
-  "26. Mother Teresa quote",
+console.log(
+  "26. Mother Teresa quote:",
   "\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\""
 );
 
-const stringTen = "10";
+let stringNumber = "10";
 
-show(
-  "27. Is typeof '10' exactly equal to 10?",
-  typeof stringTen === typeof 10 && Number(stringTen) === 10
+console.log(
+  "27. Is typeof '10' exactly equal to 10?:",
+  Number(stringNumber) === 10
 );
 
-const parsedNumber = parseFloat("9.8");
+let number = parseFloat("9.8");
 
-show(
-  "28. Make parseFloat('9.8') equal to 10",
-  Math.ceil(parsedNumber) === 10
+console.log(
+  "28. Is parseFloat('9.8') equal to 10?:",
+  Math.ceil(number) === 10
 );
 
-show(
-  "29. Is 'on' found in python and jargon?",
+console.log(
+  "29. Is 'on' found in both python and jargon?:",
   "python".includes("on") && "jargon".includes("on")
 );
 
-show(
-  "30. Is jargon in the sentence?",
+console.log(
+  "30. Is jargon in the sentence?:",
   "I hope this course is not full of jargon.".includes("jargon")
 );
 
-show(
-  "31. Random number from 0 to 100",
+console.log(
+  "31. Random number between 0 and 100:",
   Math.floor(Math.random() * 101)
 );
 
-show(
-  "32. Random number from 50 to 100",
+console.log(
+  "32. Random number between 50 and 100:",
   Math.floor(Math.random() * 51) + 50
 );
 
-show(
-  "33. Random number from 0 to 255",
+console.log(
+  "33. Random number between 0 and 255:",
   Math.floor(Math.random() * 256)
 );
 
-const javascriptWord = "JavaScript";
+let javascript = "JavaScript";
 
-const randomCharacter =
-  javascriptWord[Math.floor(Math.random() * javascriptWord.length)];
+let randomIndex = Math.floor(Math.random() * javascript.length);
 
-show(
-  "34. Random character from JavaScript",
-  randomCharacter
+console.log(
+  "34. Random character from JavaScript:",
+  javascript[randomIndex]
 );
 
-const pattern = `1 1 1 1 1
+console.log(
+  `35. Number pattern:
+1 1 1 1 1
 2 1 2 4 8
 3 1 3 9 27
 4 1 4 16 64
-5 1 5 25 125`;
-
-show("35. Number pattern", pattern);
-
-show(
-  "36. Phrase because because because",
-  becauseSentence.substr(31, 23)
+5 1 5 25 125`
 );
+
+console.log(
+  "36. Phrase because because because:",
+  sentence.substring(31, 54)
+);
+
 
 // ==================== LEVEL 3 ====================
 
-const loveSentence =
+let loveSentence =
   "Love is the best thing in this world. Some found their love and some are still looking for their love.";
 
-const loveMatches = loveSentence.match(/love/gi) || [];
+let loveWords = loveSentence.match(/love/gi);
 
-show(
-  "37. Number of times love appears",
-  loveMatches.length
+console.log(
+  "37. Number of times love appears:",
+  loveWords.length
 );
 
-const becauseMatches = becauseSentence.match(/because/gi) || [];
+let becauseWords = sentence.match(/because/gi);
 
-show(
-  "38. Number of times because appears",
-  becauseMatches.length
+console.log(
+  "38. Number of times because appears:",
+  becauseWords.length
 );
 
-const messySentence =
+let messySentence =
   "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tivat#e yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
 
-const cleanSentence = messySentence
+let cleanSentence = messySentence
   .replace(/[^a-zA-Z0-9 ]/g, "")
   .replace(/\s+/g, " ")
   .trim();
 
-const words = cleanSentence.toLowerCase().split(" ");
+console.log("39. Cleaned sentence:", cleanSentence);
 
-const wordCounts = {};
+let words = cleanSentence.toLowerCase().split(" ");
 
-words.forEach(word => {
-  wordCounts[word] = (wordCounts[word] || 0) + 1;
+let wordCount = {};
+
+words.forEach(function (word) {
+  if (wordCount[word]) {
+    wordCount[word]++;
+  } else {
+    wordCount[word] = 1;
+  }
 });
 
-const mostFrequentWord = Object.entries(wordCounts).sort(
-  (a, b) => b[1] - a[1]
-)[0];
+let mostFrequentWord = Object.keys(wordCount).sort(function (a, b) {
+  return wordCount[b] - wordCount[a];
+})[0];
 
-show(
-  "39. Cleaned sentence",
-  cleanSentence
+console.log(
+  "40. Most frequent word:",
+  mostFrequentWord,
+  "appears",
+  wordCount[mostFrequentWord],
+  "times"
 );
 
-show(
-  "40. Most frequent word and count",
-  `${mostFrequentWord[0]} (${mostFrequentWord[1]} times)`
-);
-
-const incomeText =
+let incomeSentence =
   "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
 
-const incomeNumbers = incomeText.match(/\d+/g).map(Number);
+let incomeNumbers = incomeSentence.match(/\d+/g).map(Number);
 
-const annualIncome =
+let totalAnnualIncome =
   incomeNumbers[0] * 12 +
   incomeNumbers[1] +
   incomeNumbers[2] * 12;
 
-show(
-  "41. Total annual income",
-  `${annualIncome} euro`
+console.log(
+  "41. Total annual income:",
+  totalAnnualIncome + " euro"
 );
-
-// Display all results on the webpage
-
-document.body.innerHTML = `
-  <main>
-    <h1>30 Days Of JavaScript - Day 2 Exercises</h1>
-
-    <p>
-      The answers are displayed below.
-      You can also open the browser console to see the console.log() results.
-    </p>
-
-    <section>
-      ${output.join("\n")}
-    </section>
-  </main>
-`;
-
-document.querySelectorAll("pre").forEach(pre => {
-  pre.style.whiteSpace = "pre-wrap";
-});
